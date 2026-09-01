b578d318382d959f51efbba8f4671d5e31d135bb
Iranga Deshani <irangadesh@gmail.com>
2026-09-01 15:25:20 +0530

    Update Role.java

--- src/main/java/sunrise/model/Role.java
+++ src/main/java/sunrise/model/Role.java
@@ -1,9 +1,5 @@
 package sunrise.model;
 
-/**
- * Distinguishes the two staff roles supported by the system, as modelled
- * in the Use Case diagram (Receptionist and Admin are independent actors).
- */
 public enum Role {
     RECEPTIONIST,
     ADMIN
