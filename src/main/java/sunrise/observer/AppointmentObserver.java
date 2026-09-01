5f2c348a1f3e45c09dbe4505d7aa1de4a1c26df3
Iranga Deshani <irangadesh@gmail.com>
2026-09-01 15:33:11 +0530

    Update AppointmentObserver.java

--- src/main/java/sunrise/observer/AppointmentObserver.java
+++ src/main/java/sunrise/observer/AppointmentObserver.java
@@ -2,11 +2,6 @@ package sunrise.observer;
 
 import sunrise.model.Appointment;
 
-/**
- * Observer pattern: implementations react to appointment lifecycle events
- * without AppointmentService needing to know who is listening or how many
- * listeners there are.
- */
 public interface AppointmentObserver {
 
     void onAppointmentRegistered(Appointment appointment);
